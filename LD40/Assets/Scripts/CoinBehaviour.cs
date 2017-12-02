de7b1e355d152ebecb7b9efb0d3abbfa5e163d3d
GreenCalx <romain.schoenberger@gmail.com>
2017-12-02 19:16:32 +0100

    Coins. Clean up & Folders.

---
+++ LD40/Assets/Scripts/CoinBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour {
+
+    private int __value;
+
+	// Use this for initialization
+	void Start () {
+        __value = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+    // Tries to get grabbed by the collider
+    void onTriggerEnter(Collider collider)
+    {
+        CoinGrabber coinGrabber = collider.GetComponentInParent<CoinGrabber>();
+        if (!!coinGrabber)
+            return;
+
+        coinGrabber.addCoin(__value);
+    }
+}
--- LD40/Assets/Scripts/CoinGrabber.cs
+++ LD40/Assets/Scripts/CoinGrabber.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinBehaviour : MonoBehaviour {
+public class CoinGrabber : MonoBehaviour {
+
+    private int __coinGrabbed;
 
 	// Use this for initialization
 	void Start () {
-		
+        __coinGrabbed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+    }
+
+    public void addCoin(int n_coin)
+    {
+        __coinGrabbed += n_coin;
+    }
 
 
-	}
 }
