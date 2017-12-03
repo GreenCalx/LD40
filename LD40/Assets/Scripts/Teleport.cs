127763a0cecf474c7e9cf5c92e65494e7620b444
GreenCalx <romain.schoenberger@gmail.com>
2017-12-03 16:05:46 +0100

    Teleports, colors, some unfinished animations and hub/level scenes.

---
+++ LD40/Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour {
+
+    public string sceneNameDestination;
+
+    private string __activeSceneName;
+
+	// Use this for initialization
+	void Start () {
+        __activeSceneName = SceneManager.GetActiveScene().name;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(sceneNameDestination);
+    }
+
+
+}
