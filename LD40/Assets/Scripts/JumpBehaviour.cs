d99b25a5a5bab41300e96eec08269b0da40b0722
Toffanim <marc.toffanin@gmail.com>
2017-12-03 15:02:01 +0100

    [MODIFIY] Jump Behaviour physics

--- LD40/Assets/Scripts/JumpBehaviour.cs
+++ LD40/Assets/Scripts/JumpBehaviour.cs
@@ -4,98 +4,119 @@ using UnityEngine;
 
 public class JumpBehaviour : MonoBehaviour {
 
-    public bool IsJumping { get; set; }
-    public bool HasDoubleJumped { get; set; }
-
-    public float JumpPower { get; set; }
+    // Jump Logic
+    public bool CanDoubleJump { get; set; }
     public bool AskedForJump { get; set; }
-    public bool WallSlide { get; set; }
 
-    Rigidbody2D rb;
-    public Vector3 SurfaceNormal;
+    // Physics Data
+    Rigidbody2D RB2D;
+    //   Collision ray options
+    float RayOffsetX;
+    float RayOffsetY;
+    float RayLength;
+
+    // Jump parameters
+    public float speed = 14f;
+    public float accel = 6f;
+    public float airAccel = 3f;
+    public float jump = 14f;
+    private Vector2 input;
 
     // Use this for initialization
     void Start () {
-        IsJumping = false;
         AskedForJump = false;
-        JumpPower = 15;
-        HasDoubleJumped = false;
-        rb = GetComponent<Rigidbody2D>();
+        CanDoubleJump = true;
+        RB2D = GetComponent<Rigidbody2D>();
+        BoxCollider2D collider2D = GetComponent<BoxCollider2D>();
+
+        RayOffsetX = collider2D.bounds.extents.x + 0.1f;
+        RayOffsetY = collider2D.bounds.extents.y + 0.1f;
+        RayLength = 0.01f;
     }
 
-    public void AskForJump()
+    public bool IsOnWall()
     {
-        AskedForJump = false;
-        if (!IsJumping)
-        {
-            AskedForJump = true;
-        }
+        bool HitLeft  = Physics2D.Raycast(
+            new Vector2(transform.position.x - RayOffsetX, transform.position.y), 
+                -Vector2.right, RayOffsetY);
+        bool HitRight = Physics2D.Raycast(
+            new Vector2(transform.position.x + RayOffsetX, transform.position.y), 
+                Vector2.right, RayOffsetY);
 
-        if (IsJumping && !HasDoubleJumped)
-        {
-            AskedForJump = true;
-            HasDoubleJumped = true;
-        }
-
-        if (WallSlide)
-        {
-            AskedForJump = true;
-        }
-        Debug.Log("AskedForJump : " + AskedForJump);
+        return (HitLeft || HitRight);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public bool IsGrounded()
     {
-        Debug.Log("CollisionEnter");
-        IsJumping = false;
-        HasDoubleJumped = false;
-        SurfaceNormal = collision.contacts[0].normal;
-        if (Vector2.Dot(SurfaceNormal, Vector2.up) <= 0.1)
-        { 
-            // WAll riding
-            WallSlide = true;
-        }
+        bool RayDown1 = Physics2D.Raycast(
+            new Vector2(transform.position.x, transform.position.y - RayOffsetY), 
+               -Vector2.up, RayLength);
+        bool RayDown2 = Physics2D.Raycast(
+            new Vector2(transform.position.x + (RayOffsetX - 0.2f), transform.position.y - RayOffsetY), 
+               -Vector2.up, RayLength);
+        bool RayDown3 = Physics2D.Raycast(
+            new Vector2(transform.position.x - (RayOffsetX - 0.2f), transform.position.y - RayOffsetY), 
+               -Vector2.up, RayLength);
+        return (RayDown1 || RayDown2 || RayDown3);
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    //Returns whether or not player is touching wall or ground.
+    public bool IsTouchingSomething()
     {
-        Debug.Log("CollisionExit");
-        IsJumping = true;
-        WallSlide = false;
+        return (IsGrounded() || IsOnWall());
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    //Returns direction of wall.
+    public int wallDirection()
     {
-        IsJumping = false;
-        HasDoubleJumped = false;
-        SurfaceNormal = collision.contacts[0].normal;
-        if (Vector2.Dot(SurfaceNormal, Vector2.up) <= 0.1)
-        {
-            // WAll riding
-            WallSlide = true;
-        }
+        bool left = Physics2D.Raycast(
+            new Vector2(transform.position.x - RayOffsetX, transform.position.y), 
+               -Vector2.right, RayLength);
+        bool right = Physics2D.Raycast(
+            new Vector2(transform.position.x + RayOffsetX, transform.position.y), 
+            Vector2.right, RayLength);
+
+        if (left)
+            return -1;
+        else if (right)
+            return 1;
+        else
+            return 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            input.x = -1;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            input.x = 1;
+        else
+            input.x = 0;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            input.y = 1;
     }
 
     // Update is called once per frame
     void FixedUpdate () {
-        Debug.Log(WallSlide + " " + IsJumping + " " + HasDoubleJumped);
-        if(AskedForJump)
-        {
-            if (WallSlide)
-            {
-                rb.AddForce(new Vector2(SurfaceNormal.x*100, SurfaceNormal.y), ForceMode2D.Impulse);
-            }
-            else
-            {
-                rb.AddForce(new Vector2(0, SurfaceNormal.y * JumpPower), ForceMode2D.Impulse);
-            }
-            AskedForJump = false;
-            return;
-        }
+        if (IsGrounded())
+            CanDoubleJump = true;
 
-        if (WallSlide)
-        {
-            rb.velocity = new Vector2(0, -0.7f);
-        }
+        Debug.Log(CanDoubleJump);
+
+        RB2D.AddForce(new Vector2(((input.x * speed) - RB2D.velocity.x) * (IsGrounded() ? accel : airAccel), 
+                                    0)); 
+
+        RB2D.velocity = new Vector2((input.x == 0 && IsGrounded()) ? 0 : RB2D.velocity.x, 
+                                    (input.y == 1 && (IsTouchingSomething() || CanDoubleJump)) ? jump : RB2D.velocity.y);
+
+        if (input.y == 1 && !IsTouchingSomething() && CanDoubleJump) CanDoubleJump = false;
+
+        if (IsOnWall() && !IsGrounded() && input.y == 1)
+            RB2D.velocity = new Vector2(-wallDirection() * speed * 0.75f, RB2D.velocity.y);
+
+        Debug.Log(CanDoubleJump);
+
+        input.y = 0;
     }
 }
--- LD40/Assets/Scripts/PlayerController.cs
+++ LD40/Assets/Scripts/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour {
     {
         if (Input.GetKeyDown(JumpKey))
         {
-            jump.AskForJump();
+            //jump.AskForJump();
         }
         return;
     }
@@ -31,38 +31,6 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         float Movement = Input.GetAxis("Horizontal");
-        /*   if (jump.WallSlide)
-           {
-               Vector2 MovementVector = new Vector2(Movement * behaviour._PlayerSpeed, rb.velocity.y);
-               if (Vector2.Dot(jump.SurfaceNormal, MovementVector) > 0) {
-                   Debug.Log("hello");
-                   rb.velocity = new Vector2(Movement * behaviour._PlayerSpeed, rb.velocity.y);
-               }
-           }
-           else */
-        /*     {
-
-                 if (!jump.WallSlide)
-                 {
-                     if (Movement * rb.velocity.x < behaviour.PlayerMaxSpeed)
-                         rb.AddForce(Vector2.right * Movement * behaviour.PlayerSpeed);
-
-                     if (Mathf.Abs(rb.velocity.x) > behaviour.PlayerMaxSpeed)
-                         rb.velocity = new Vector2(Mathf.Sign(Movement) * behaviour.PlayerMaxSpeed, rb.velocity.y);
-
-                     if (Movement == 0)
-                         rb.AddForce(-rb.velocity);
-                 }
-                 else
-                 {
-                     rb.velocity = new Vector2(0, 0);
-                 }
-                 //Debug.Log(Movement);
-                 //Debug.Log(rb.velocity.x);
-
-             }
-             */
-             if(!jump.WallSlide)
-                 rb.velocity = new Vector2(Movement * behaviour.PlayerMaxSpeed, rb.velocity.y);
+        
     }
 }
