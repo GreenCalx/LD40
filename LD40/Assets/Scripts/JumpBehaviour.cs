6dff77e06e0ee08af0311c03da2e3ccaa3877428
Toffanim <marc.toffanin@gmail.com>
2017-12-03 02:06:19 +0100

    [MODIFY] JumpBahaviour

--- LD40/Assets/Scripts/JumpBehaviour.cs
+++ LD40/Assets/Scripts/JumpBehaviour.cs
@@ -7,12 +7,12 @@ public class JumpBehaviour : MonoBehaviour {
     public bool IsJumping { get; set; }
 
     public float JumpPower { get; set; }
-    private bool StartJump;
+    public bool AskForJump { get; set; }
 	// Use this for initialization
 	void Start () {
         IsJumping = false;
-        StartJump = false;
-        JumpPower = 8;
+        AskForJump = false;
+        JumpPower = 800;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -30,6 +30,13 @@ public class JumpBehaviour : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
+        Debug.Log(AskForJump);
+        if(AskForJump)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2(0,JumpPower));
+            AskForJump = false;
+        }
 
     }
 }
--- LD40/Assets/Scripts/PlayerController.cs
+++ LD40/Assets/Scripts/PlayerController.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private bool _IsMenuMode;
-
-	// Use this for initialization
-	void Start () {
+    Rigidbody2D rb;
+    JumpBehaviour jump;
+    // Use this for initialization
+    void Start () {
         _IsMenuMode = false;
-	}
+         rb = GetComponent<Rigidbody2D>();
+        jump = GetComponent<JumpBehaviour>();
+    }
+
+    private void Update()
+    {
+        if (!jump.IsJumping) {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                jump.AskForJump = true;
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
         Transform transform = GetComponent<Transform>();
         PlayerBehaviour behavior = GetComponent<PlayerBehaviour>();
         if(transform) {
             float Movement = Input.GetAxis("Horizontal");
             rb.velocity = new Vector2(Movement * behavior._PlayerSpeed, rb.velocity.y);              
-
-            if (Input.GetKeyDown(KeyCode.UpArrow)) {
-                JumpBehaviour jump = GetComponent<JumpBehaviour>();
-                if(!jump.IsJumping)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, jump.JumpPower);
-                }
-                
-                //transform.Translate(_DirectionUp * behavior._PlayerSpeed);
-            }
-
         }
 	}
 }
