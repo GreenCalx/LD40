3d81831e9f70abe50e40374c8680b6bf1ab17ee7
GreenCalx <romain.schoenberger@gmail.com>
2017-12-05 01:26:02 +0100

    Add boss

---
+++ LD40/Assets/Scripts/KillableByWeakPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillableByWeakPoint : KillableByBloc {
+
+    public Collider2D WeakSpot;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //Debug.Log("TRIGGER ENTER KILLABLEBYBLOC");
+        GameObject go = collider.gameObject;
+        if (go)
+        {
+            if (go.GetComponent<BasePlateform>())
+            {
+                EnnemyBehaviour behaviour = GetComponent<EnnemyBehaviour>();
+                if (behaviour &&  WeakSpot.IsTouching(collider) )
+                {
+                    behaviour.Kill();
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
